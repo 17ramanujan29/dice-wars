0edc24e9b226bbfc7c73afe14906e344e7cc0e94
Ramanujan <mrxeuler314@gmail.com>
2026-09-13 14:03:25 +0900

    コード分割終了

---
+++ src/models/Player.js
@@ -0,0 +1,7 @@
+export class Player {
+    constructor(id, color, name) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+    }
+}
---
+++ src/utils/hexUtils.js
@@ -0,0 +1,48 @@
+import { CONFIG } from '../config.js';
+
+export function getHexMetrics(hexSize) {
+    return {
+        hexHeight: Math.sqrt(3) * hexSize,
+        hexWidth: 2 * hexSize,
+        vertDist: Math.sqrt(3) * hexSize,
+        horizDist: 2 * hexSize * 0.75
+    };
+}
+
+export function getHexCenter(col, row, hexSize) {
+    const metrics = getHexMetrics(hexSize);
+    let x = col * metrics.horizDist;
+    let y = row * metrics.vertDist;
+    if (col % 2 !== 0) y += metrics.vertDist / 2;
+    return { x, y };
+}
+
+export function pixelToHex(px, py, hexSize) {
+    const q = (2/3 * px) / hexSize;
+    const r = (-1/3 * px + Math.sqrt(3)/3 * py) / hexSize;
+    return hexRound(q, r, -q-r);
+}
+
+function hexRound(q, r, s) {
+    let rq = Math.round(q), rr = Math.round(r), rs = Math.round(s);
+    const q_diff = Math.abs(rq - q), r_diff = Math.abs(rr - r), s_diff = Math.abs(rs - s);
+    if (q_diff > r_diff && q_diff > s_diff) rq = -rr - rs;
+    else if (r_diff > s_diff) rr = -rq - rs;
+    else rs = -rq - rr;
+    return { col: rq, row: rr + (rq - (rq & 1)) / 2 };
+}
+
+export function getValidNeighbors(col, row) {
+    const neighbors = [];
+    const dirs = [
+        [[+1,  0], [+1, -1], [ 0, -1], [-1, -1], [-1,  0], [ 0, +1]], 
+        [[+1, +1], [+1,  0], [ 0, -1], [-1,  0], [-1, +1], [ 0, +1]]  
+    ];
+    for (let d of dirs[col & 1]) {
+        const nc = col + d[0], nr = row + d[1];
+        if (nc >= 0 && nc < CONFIG.gridWidth && nr >= 0 && nr < CONFIG.gridHeight) {
+            neighbors.push({col: nc, row: nr});
+        }
+    }
+    return neighbors;
+}
